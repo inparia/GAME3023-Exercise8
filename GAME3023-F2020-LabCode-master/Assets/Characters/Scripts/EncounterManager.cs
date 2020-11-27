4fa044d3546dde3b24f7c2d0757502eada2329d8
inparia <inparia@live.ca>
2020-11-27 18:42:36 -0500

    Initial Upload

---
+++ GAME3023-F2020-LabCode-master/Assets/Characters/Scripts/EncounterManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EncounterManager : MonoBehaviour
+{
+    public void EnterEncounter()
+    {
+        SceneManager.LoadScene("BattleScene");
+    }
+
+    public void ExitEncounter()
+    {
+        // In a full game, your code should remember the player's last area and return there
+        SceneManager.LoadScene("Overworld");
+    }
+}
